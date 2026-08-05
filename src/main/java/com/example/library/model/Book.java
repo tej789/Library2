2c72f9d9179fa004e5182d8ecbd8f2ffe5cc416a
tej789 <gotitej2005@gmail.com>
2026-08-05 17:33:18 +0530

    test

--- src/main/java/com/example/library/model/Book.java
+++ src/main/java/com/example/library/model/Book.java
@@ -1,7 +1,32 @@
 package com.example.library.model;
-
 public class Book {
-
     private int id;
-    private String Title;
+    private String title;
+    private String writer;
+    private double price;
+
+    public Book(int id, String title , String writer ,double price) {
+        this.id = id;
+        this.title = title;
+        this.writer = writer;
+        this.price = price;
+    }
+    public int getId(){
+        return id;
+    }
+    public String getTitle(){
+        return title;
+    }
+
+    public String getWriter(){
+        return writer;
+    }
+    public double getPrice(){
+        return price;
+    }
+    @Override
+    public String toString() {
+        String x =  "\nID : " + id + "  Title : " + title + "  Writer: " + writer + "  Price : " + price;
+        return x;
+    }
 }
